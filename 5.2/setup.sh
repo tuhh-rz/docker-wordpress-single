bbc9bc93eb2ad5c6f7c57710b216001c0211da6a
Andreas Böttger <andreas.boettger@tuhh.de>
2019-06-24 15:38:34 +0000

    Update setup.sh

--- 5.2/setup.sh
+++ 5.2/setup.sh
@@ -123,6 +123,10 @@ if [[ $ENABLE_SSL == true ]]; then
     fi
 fi
 
+# consider a table prefix
+export TABLE_PREFIX=${TABLE_PREFIX:-wp_}
+perl -pe 's/^(\$table_prefix\s+=\s+).*/\1\x27$ENV{'TABLE_PREFIX'}\x27;/g' /var/www/html/${RELATIVE_PATH}/wp-config.php
+
 chown -Rf www-data.www-data /var/www/html/
 
 find /var/www/html -type f -print0 | xargs -0 chmod 660
